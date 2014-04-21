9e1cc6db2944c46c2a59d7d1741959fefb1a9e0e
yunbaly <yunbaly@gmail.com>
2014-04-21 17:05:04 +0900

    git commit

---
+++ src/com/pepperoni/idea/HelloGit.java
@@ -0,0 +1,14 @@
+package com.pepperoni.idea;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 70140231
+ * Date: 14. 4. 21
+ * Time: 오후 4:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloGit {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Hello Git!!!");
+    }
+}
